a07df21ae6f7e321f1cdf372a6b10e1d421225da
neftalyy <asafneftaly@gmail.com>
2024-09-02 09:35:32 -0600

    Actividad 3

--- p20_ConversionTipos.java
+++ p20_ConversionTipos.java
@@ -1,15 +1,20 @@
-// Conversion entre tipos de datos en Java
+//Conversión
+
+import java.util.Scanner;
+
 public class p20_ConversionTipos {
     public static void main(String[] args) {
-    // Conversión implicita: de pequeño a grande
+    
     int var1 = 9;
-    double var2 = var1; // int a double
+    double var2 = var1; 
     float var3 = 123456.789f;
-    double var4 = var3; // float a double
+    double var4 = var3; 
+
     System.out.print("\033[H\033[2J"); System.out.flush();
+    
     System.out.println(String.format("Implicita de int a double : %d %f",var1,var2));
     System.out.println(String.format("Implicita de float a double : %f %f",var3,var4));
-    // Conversiones explicitas : de grande a pequeño
+   
     long var5 = 1234567890123456L;
     int var6 = (int) var5;
     int var7 = 21448364;
